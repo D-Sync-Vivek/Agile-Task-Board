d0e0af1ddca71d4b16d503334060cfab8931d1fe
D-Sync-Vivek <vivekpat931@gmail.com>
2025-12-28 14:00:17 +0530

    Now columns are draggable

--- components/Kanban/ColumnContainer.tsx
+++ components/Kanban/ColumnContainer.tsx
@@ -1,24 +1,44 @@
 "use client"
 import { Column } from "@/types"
 import { useKanbanStore } from "@/store/useKanbanStore"
-const ColumnContainer = ({column}: {column: Column}) => {
+import { useSortable } from "@dnd-kit/sortable"
+import { CSS } from "@dnd-kit/utilities"
 
-  const deleteColumn = useKanbanStore((state) => state.deleteColumn)
-  const tasks = useKanbanStore((state) => state.tasks)
-  
-  function callDeleteColumn(){
-    const id = column.id
-    deleteColumn(id)
-  }
-  return (
-    <div className="bg-gray-100 w-87.5 p-3">
-      <span>{column.title}</span>
-      {tasks.filter(task => task.columnId === column.id).map((task) => (
-        <p key={task.columnId}>{task.content}</p>
-      ))}
-      <span onClick={callDeleteColumn}>🗑️</span>
-    </div>
-  )
+const ColumnContainer = ({ column }: { column: Column }) => {
+
+    const deleteColumn = useKanbanStore((state) => state.deleteColumn)
+    const tasks = useKanbanStore((state) => state.tasks)
+
+    const { setNodeRef, attributes, listeners, transform, transition, isDragging } = useSortable({
+        id: column.id,
+        data: {
+            type: "Column",
+            column
+        }
+    })
+
+    const style = {
+        transform: CSS.Transform.toString(transform),
+        transition
+    }
+
+    function callDeleteColumn() {
+        const id = column.id
+        deleteColumn(id)
+    }
+    return (
+        <>
+            <div className="bg-gray-100 flex p-2" ref={setNodeRef} style={style}>
+                <div className={`w-87.5 ${isDragging === true ? "border-2 border-rose-500 opacity-50" : ""}`}   {...attributes} {...listeners}>
+                <span className="">{column.title}</span>
+                {tasks.filter(task => task.columnId === column.id).map((task) => (
+                    <p key={task.id}>{task.content}</p>
+                ))}
+            </div>
+            <span className="flex items-center" onClick={callDeleteColumn}>🗑️</span>
+            </div>
+        </>
+    )
 }
 
 export default ColumnContainer
--- components/Kanban/KanbanBoard.tsx
+++ components/Kanban/KanbanBoard.tsx
@@ -3,21 +3,42 @@ import { useKanbanStore } from "@/store/useKanbanStore"
 import { DndContext } from "@dnd-kit/core";
 import { SortableContext } from "@dnd-kit/sortable";
 import ColumnContainer from "./ColumnContainer";
+import { DragEndEvent } from "@dnd-kit/core";
+import { arrayMove } from "@dnd-kit/sortable";
+import { useState } from "react";
 
 const KanbanBoard = () => {
     const columns = useKanbanStore((state) => state.columns)
-    const addColumn = useKanbanStore((state) => state.addColumn);
-    
+    const addColumn = useKanbanStore((state) => state.addColumn)
+    const columnsIDs = columns.map((column) => column.id)
+    const setColumns = useKanbanStore((state) => state.setColumns)
+    const [count, setCount] = useState(1)
+
     function callAddColumn() {
-        addColumn(`Hey there`);
+        addColumn(`Hey there ${count}`);
+        setCount((count => count + 1))
+    }
+
+    function onDragEnd(event: DragEndEvent) {
+        const {active, over} = event;
+        if(!over) return ;
+        if(active.id === over.id) return;
+
+        const oldIndex = columns.findIndex((col) => col.id === active.id)
+        const newIndex = columns.findIndex((col) => col.id === over.id);
+        setColumns(arrayMove(columns, oldIndex, newIndex));
     }
     return (
-        <div className="m-auto flex min-h-screen w-full items-center overflow-x-auto overflow-y-hidden px-10">
+        <div className="m-auto flex mt-5 min-h-screen w-full items-start overflow-x-auto overflow-y-hidden px-10">
             <div className="flex gap-4">
-                {columns.map((column) => (
-                    <ColumnContainer key={column.id} column={column}/>
-                ))}
-                <button onClick={callAddColumn}>Add Column</button>
+                <DndContext onDragEnd={onDragEnd}>
+                    <SortableContext items={columnsIDs}>
+                        {columns.map((column) => (
+                            <ColumnContainer key={column.id} column={column} />
+                        ))}
+                    </SortableContext>
+                </DndContext>
+                <button className="border px-2 p-1 rounded-md" onClick={callAddColumn}>Add Column</button>
             </div>
         </div>
     )
